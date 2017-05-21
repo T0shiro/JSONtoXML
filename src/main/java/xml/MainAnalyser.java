a3eadfcd9eca1889879954d1210a78a1adaae698
Thomas <thomas.canava@etu.unice.fr>
2017-05-21 15:38:37 +0200

    projet terminé

--- src/main/java/xml/MainAnalyser.java
+++ src/main/java/xml/MainAnalyser.java
@@ -6,20 +6,23 @@ import java.util.List;
  * Created by Thomas on 20/05/2017.
  */
 public class MainAnalyser {
+    
+    public static final String DEVIATION = "Ecart-type du coût: ";
+    
     public static void main(String[] args) {
         XMLParser xmlParser = new XMLParser();
         List<Action> actions = xmlParser.parse("file.xml");
         Analyser analyser = new Analyser();
         System.out.println("Nombres d'actions: " + actions.size());
-        System.out.println("Moyenne: " + analyser.meanCost(actions));
-        System.out.println("Ecart-type: " + analyser.deviationCost(actions));
+        System.out.println("Coût moyen: " + analyser.meanCost(actions));
+        System.out.println(DEVIATION + analyser.deviationCost(actions));
         List<List<Action>> actionsList = analyser.separateByType(actions);
         for (List<Action> actionList : actionsList) {
             System.out.println("\n=========================================================\n");
             String name = actionList.get(0).getName();
             System.out.println("Nombre de " + name + ": " + actionList.size());
-            System.out.println("Moyenne de " + name + ": " + analyser.meanCost(actionList));
-            System.out.println("Ecart-type de " + name + ": " + analyser.deviationCost(actionList));
+            System.out.println("Coût moyen de " + name + ": " + analyser.meanCost(actionList));
+            System.out.println(DEVIATION + " de " + name + ": " + analyser.deviationCost(actionList));
         }
     }
 }
